8de2334ed1e6923ba14ec341879917df1aedf928
Alexandre Eher <alexandre.eher@werkspot.nl>
2016-10-11 16:53:10 +0200

    Update publish script

--- publi.sh
+++ publi.sh
@@ -5,10 +5,17 @@ if [ $# -ne 1  ]; then
     exit 1;
 fi
 
+git stash
+mv *.html generated
+mv *.css generated
+mv *.rss generated
 git checkout gh-pages
 
+mv generated/* .
+rm -rf generated
 git add -A .
 git commit -m "$1"
 git push origin gh-pages
 
 git checkout master
+git stash pop
