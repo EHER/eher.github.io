eb802ce712c9461db4db02078bf2f1a943053a81
Alexandre Eher <alexandre.eher@werkspot.nl>
2016-10-11 16:30:02 +0200

    Create script to publish on Github pages

---
+++ publi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 1  ]; then
+    echo "usage: $0 \"commit message\""
+    exit 1;
+fi
+
+git stash
+git checkout gh-pages
+
+git add -A .
+git commit -m "$1"
+git push origin gh-pages
+
+git checkout master
+git stash pop
